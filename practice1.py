7af4f6341516bdd6dcfb8dcb6bea671656814da6
Prashant Shrestha <prashantsht700@gmail.com>
2024-01-03 18:24:31 +0545

    Modified

--- practice1.py
+++ practice1.py
@@ -1,1 +1,22 @@
 print("Hello world")
+a = 1 + 2 * 3 / 4.0
+print(a)
+
+remainder = 11 % 3
+print(remainder)
+
+square = 4 ** 2
+cube = 4 ** 3
+print(square)
+print(cube)
+
+a = "hello" + " " + "world"
+print(a)
+
+b = "hello" * 5
+print(b)
+
+name = "Ram"
+print("Hello %s" % name)
+age = 23
+print("%s is %d years old" %(name, age))
